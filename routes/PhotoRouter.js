bb428f09c725cfb08331c7d1783c413c46f5abea
Lê Trọng Tuyên <85683857+tuyenlt@users.noreply.github.com>
2025-05-18 05:19:26 +0000

    Initial commit

---
+++ routes/PhotoRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const Photo = require("../db/photoModel");
+const router = express.Router();
+
+router.post("/", async (request, response) => {
+  
+});
+
+router.get("/", async (request, response) => {
+  
+});
+
+module.exports = router;
