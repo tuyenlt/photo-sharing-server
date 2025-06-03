f6462b1901e0eb5dd6d5e3d686aa2e4f2397a4d3
Lê Trọng Tuyên <85683857+tuyenlt@users.noreply.github.com>
2025-06-03 09:56:33 +0000

    update

--- src/index.js
+++ src/index.js
@@ -9,20 +9,25 @@ const path = require("path");
 dbConnect();
 
 app.use((req, res, next) => {
-	console.log(`${req.method} request for '${req.url}'`);
-	next();
-})
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 app.use(express.json());
 app.use("/api/uploads", express.static(path.join(__dirname, "../uploads")));
 
-app.use("/api", router)
+app.use("/api", router);
 
 app.get("/", (request, response) => {
-	response.send({ message: "Hello from photo-sharing app API!" });
+  response.send({ message: "Hello from photo-sharing app API!" });
 });
 
 app.listen(3000, () => {
-	console.log("server listening on port 3000");
+  console.log("server listening on port 3000");
 });
