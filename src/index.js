f112dba1bab4cee880bb9ebb96aa6d98d9669e35
tuyenlt <keplerlu@gmail.com>
2025-06-03 17:08:27 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -4,30 +4,26 @@ const cors = require("cors");
 const dbConnect = require("./db/dbConnect");
 const router = require("./routes");
 const path = require("path");
-// const LoadDb = require("./db/dbLoad");
 
 dbConnect();
 
 app.use((req, res, next) => {
-  console.log(`${req.method} request for '${req.url}'`);
-  next();
+	console.log(`${req.method} request for '${req.url}'`);
+	next();
 });
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
+// Allow all origins and handle preflight
+app.use(cors());
+app.options("*", cors());
+
 app.use(express.json());
 app.use("/api/uploads", express.static(path.join(__dirname, "../uploads")));
-
 app.use("/api", router);
 
-app.get("/", (request, response) => {
-  response.send({ message: "Hello from photo-sharing app API!" });
+app.get("/", (req, res) => {
+	res.send({ message: "Hello from photo-sharing app API!" });
 });
 
 app.listen(3000, () => {
-  console.log("server listening on port 3000");
+	console.log("server listening on port 3000");
 });
